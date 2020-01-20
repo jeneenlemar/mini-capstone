ce0c3972b6ce2a2779865a52313829c6e3c9cc74
Jeneen <jeneen.lemar@gmail.com>
2020-01-20 18:17:59 -0500

    adds REST items for index and show to include routes, controller methods, and views

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,22 +1,66 @@
 class Api::ProductsController < ApplicationController
 
-  def all_products_method
+  
+  def index
     @products = Product.all
-    render "all_products.json.jb"
+    render "index.json.jb"
   end
 
-  def product_method
-    @product = Product.first
-    render "product.json.jb"
+  def show
+    @product = Product.find_by(id: params["id"])
+    render "show.json.jb"
   end
 
+  #Note - I am going to RENDER to the show file since I want to see the same things as I did for the individual product!!!!
 
+  def create
+    @product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      description: params["description"],
+      image_url: params["image_url"]
+      )
+    @product.save
 
-  def product_query_method
-    @my_query = params["id"]
-    @product = Product.find_by(id: @my_query)
-    render "product_query.json.jb"
+    render "show.json.jb"
   end
+
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.image_url = params[:image_url] || @product.image_url
+
+    @product.save
+
+    render "show.json.jb"
+
+  end
+
+  def destroy
+    @product = Product.find_by(id: params["id"])
+    @product.destroy
+    render json: {message: "item successfully deleted"}
+  end
+
+  # def all_products_method
+  #   @products = Product.all
+  #   render "all_products.json.jb"
+  # end
+
+  # def product_method
+  #   @product = Product.first
+  #   render "product.json.jb"
+  # end
+
+
+
+  # def product_query_method
+  #   @my_query = params["id"]
+  #   @product = Product.find_by(id: @my_query)
+  #   render "product_query.json.jb"
+  # end
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,24 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
 
   namespace :api do
-    get "/all_products_url" => "products#all_products_method"
-    get "/product_url" => "products#product_method"
-    get "/product2_url" => "products#product_method"
-    get "/product_query_url" => "products#product_query_method"
+
+#make your all products route, method, view and test basics! - done
+#repeat process for show - done
+# repeat process for post - done
+# repeat for update - done
+# repeat for delete - 
+
+    get "/products" => "products#index"
+    get "/products/:id" => "products#show"
+    post "/products" => "products#create"
+    patch "products/:id" => "products#update"
+    delete "products/:id" => "products#destroy"
+
+
+    # get "/all_products_url" => "products#all_products_method"
+    # get "/product_url" => "products#product_method"
+    # get "/product2_url" => "products#product_method"
+    # get "/product_query_url" => "products#product_query_method"
    
 
 
